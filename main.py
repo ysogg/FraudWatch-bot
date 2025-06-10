a551f9c952809d61ba81416c007c5f42b6a5ac1e
ysogg <ajb.personal@hotmail.com>
2025-06-10 17:17:26 -0400

    Remove cmd + epoch tracking

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 
 import os
 from dotenv import load_dotenv
+import pandas as pd
+import csv
+from datetime import datetime
 
 import discord
 from discord.ext import commands
@@ -15,10 +18,32 @@ intents.message_content = True
 
 bot = commands.Bot(command_prefix='$', intents=intents)
 
+
+def logUserEpoch(user):
+    with open('epochs.csv', 'a', newline='') as csvfile:
+        writer = csv.writer(csvfile, delimiter=',')
+        writer.writerow([str(user), datetime.today().strftime('%Y-%m-%d %H:%M:%S')])
+
 @bot.command()
-async def fraud(ctx, name: discord.User):
+async def fraud(ctx, user: discord.User):
     role = ctx.guild.get_role(1381733158286004396) #Role ID
-    await name.add_roles(role)
+    await user.add_roles(role)
+    logUserEpoch(user)
+
+@bot.command()
+async def rmfraud(ctx, user: discord.User):
+    role = ctx.guild.get_role(1381733158286004396)
+    await user.remove_roles(role)
+
+    df = pd.read_csv('epochs.csv')
+
+    df_s = df
+
+    df_s.set_index('user', inplace=True)
+    
+    df_s = df_s.drop(str(user))
+
+    df_s.to_csv('epochs.csv')
 
 @bot.event
 async def on_ready():
